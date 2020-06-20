2a01552b7c6345ca28591184a3c65b56bc8b0e3a
Pozakhmarnyi <57231356+Pozakhmarnyi@users.noreply.github.com>
2020-06-20 15:19:58 +0300

    New removeOneMainList WORK ! v.0.54

--- src/ToDo.js
+++ src/ToDo.js
@@ -4,17 +4,23 @@ import MainLists from "./components/MainLists";
 import { useState } from "react";
 import WrapperTodoList from "./WrapperTodoList";
 
-const todoLists = [
-  { id: 5, mainTitle: "Make ToDo app" },
-  { id: 6, mainTitle: "Справи на тиждень" },
-  { id: 7, mainTitle: "Домашні справи" },
-];
-
 function ToDo() {
+  const [todoLists, setTodoLists] = useState([
+    { id: 5, mainTitle: "Make ToDo app" },
+    { id: 6, mainTitle: "Справи на тиждень" },
+    { id: 7, mainTitle: "Домашні справи" },
+  ]);
+
+  function removeOneMainList(id) {
+    setTodoLists(todoLists.filter((oneOfList) => oneOfList.id !== id));
+  }
   return (
     <div className="mainbox">
       <div className="lists_left_side">
-        <MainLists todoLists={todoLists} />
+        <MainLists
+          todoLists={todoLists}
+          removeOneMainList={removeOneMainList}
+        />
       </div>
 
       <WrapperTodoList />
--- src/components/MainLists.js
+++ src/components/MainLists.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function MainLists({ todoLists }) {
+function MainLists({ todoLists, removeOneMainList }) {
   console.log(todoLists);
 
   const oneLists = todoLists.map((oneOfList) => (
     <li key={oneOfList.id}>
       {oneOfList.mainTitle}
-      <button>
+      <button onClick={() => removeOneMainList(oneOfList.id)}>
         <i className="fas fa-trash-alt"></i>
       </button>
     </li>
