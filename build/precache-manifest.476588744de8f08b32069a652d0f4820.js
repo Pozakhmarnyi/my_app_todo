88e3d5c2acf38b4dd20196535fb21b3eac92f8af
Pozakhmarnyi <57231356+Pozakhmarnyi@users.noreply.github.com>
2020-07-22 15:32:57 +0300

    buid +

---
+++ build/precache-manifest.476588744de8f08b32069a652d0f4820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f73be709175c0b9ee501255c43d4cd9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c5455579efd886214739",
+    "url": "./static/css/main.4802353d.chunk.css"
+  },
+  {
+    "revision": "e453e69774d61b6ee061",
+    "url": "./static/js/2.b26357ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b26357ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5455579efd886214739",
+    "url": "./static/js/main.6179a084.chunk.js"
+  },
+  {
+    "revision": "235827dbcd677f4890a5",
+    "url": "./static/js/runtime-main.2a5e45ea.js"
+  }
+]);
